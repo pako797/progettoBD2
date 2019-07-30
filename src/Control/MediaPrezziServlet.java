fe3c8c9d320c86179327462f1e6d3851e4048908
pako797 <paquiprisco@gmail.com>
2019-07-30 19:53:46 +0200

    dffr

--- src/Control/MediaPrezziServlet.java
+++ src/Control/MediaPrezziServlet.java
@@ -26,7 +26,7 @@ public class MediaPrezziServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub r
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
